d991d064d5dfed54b4bfb96a6b86c1f8f4721fb8
GilmanMaxim <maxim.gilman@yandex.ru>
2021-10-09 00:06:05 +0300

    Add 'create user' form

--- JapaneseLessons/Forms/MainForm.cs
+++ JapaneseLessons/Forms/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using JapaneseLessons.Forms.Users;
 using JapaneseLessons.Models;
 using JapaneseLessons.Repositories;
 using JapaneseLessons.Services;
